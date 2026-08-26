9d51904cc11c08d262bc7ad8ea0f8b9688a9a8e4
Dellah <dellah@example.com>
2026-08-26 17:47:09 +0000

    Bump cache v22

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'mes-finances-v21';
+const CACHE_NAME = 'mes-finances-v22';
 const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png', './vendor/chart.umd.min.js'];
 
 self.addEventListener('install', event => {
